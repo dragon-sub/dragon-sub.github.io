deaa35ed471c8f004bc563eefd3dd554ddd0bd20
dragon_sub <185894681+dragon-sub@users.noreply.github.com>
2025-10-25 08:01:47 +0900

    カード化

--- blog/blog.js
+++ blog/blog.js
@@ -13,11 +13,11 @@ function render(posts) {
       container.innerHTML += `
         <br>
         <div class="post-summary">
-          <a href="#k${post.id}" class="blog-card">
-            <div style="display: justify-content: space-between; align-items: center; flex-direction: row;">
+          <a href="#k${post.id}" id="blog-card" style="border-radius: 15px;">
+            <div style="display: flex; justify-content: space-between; align-items: center; flex-direction: row; display: inline-block;">
               <div style="display: flex; flex-direction: column;">
-                <div style="font-size: 20px; margin: 10px 0px 5px 10px;">${post.title}</div>
-                <div style="font-size: 16px: margin: 5px 0px 10px 10px;">${post.date}</div>
+                <div style="font-size: 20px; padding: 10px 0px 5px 10px;">${post.title}</div>
+                <div style="font-size: 16px: padding: 5px 0px 10px 10px;">${post.date}</div>
               </div>
               <div style="background-color: #f3f4f6; border: 2px solid #e5e7eb; border-radius: 10px; font-size: 16px;">約${Math.round(post.content.length / 500)}分</div>
             </div>
@@ -53,6 +53,7 @@ fetch("blog.json")
     render(posts);
     window.addEventListener("hashchange", () => render(posts));
   });
+
 
 
 
